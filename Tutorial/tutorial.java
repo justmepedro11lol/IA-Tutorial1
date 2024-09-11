ae690aff8dcd5e011af4188002d6a7c753cda93a
justmepedro11lol <justmepedro11lol@gmail.com>
2024-09-11 11:16:56 +0100

    update

--- Tutorial/tutorial.java
+++ Tutorial/tutorial.java
@@ -1,1 +1,16 @@
-    Q
+import java.util.List;
+
+interface Ilayout {
+    /**
+    @return the children of the receiver.
+    */
+    List<Ilayout> children();
+    /**
+    @return true if the receiver equals the argument l; return false otherwise.
+    */
+    boolean isGoal(Ilayout l);
+    /**
+    @return the cost from the receiver to a successor
+    */
+    double getK();
+    }
