d31941924c33ce3cb466abe2e3d0618713b1a28d
LuisR <luis.rosa17@hotmail.com>
2024-09-11 11:20:33 +0100

    Update tutorial.java

--- Tutorial/tutorial.java
+++ Tutorial/tutorial.java
@@ -1,16 +1,54 @@
-import java.util.List;
 
 interface Ilayout {
+
     /**
-    @return the children of the receiver.
-    */
+     * @return the children of the receiver.
+     */
     List<Ilayout> children();
+
     /**
-    @return true if the receiver equals the argument l; return false otherwise.
-    */
+     * @return true if the receiver equals the argument l; return false
+     * otherwise.
+     */
     boolean isGoal(Ilayout l);
+
     /**
-    @return the cost from the receiver to a successor
-    */
-    double getK();
+     * @return the cost for moving from the input config to the receiver.
+     */
+    double getG();
+}
+
+class Board implements Ilayout, Cloneable {
+
+    private static final int dim = 3;
+    private int board[][];
+
+    public Board() {
+        board = new int[dim][dim];
     }
+
+    public Board(String str) throws IllegalStateException {
+        if (str.length() != dim * dim) {
+            throw new IllegalStateException("Invalid arg in Board constructor");
+        }
+        board = new int[dim][dim];
+        int si = 0;
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
+                board[i][j] = Character.getNumericValue(str.charAt(si++));
+            }
+        }
+    }
+
+    public String toString() {
+// TO BE COMPLETED
+    }
+
+    public boolean equals(Object o) {
+// TO BE COMPLETED
+    }
+
+    public int hashCode() {
+// TO BE COMPLETED
+    }
+//… TO BE COMPLETED
